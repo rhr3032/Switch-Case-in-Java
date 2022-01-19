24f920ecb01ac453ffcc290f5d22297d4548fcea
Raisul Hasan Rafi <raisulhasanrafi3032@gmail.com>
2022-01-19 10:36:42 -0800

    committed all file

---
+++ app.java
@@ -0,0 +1,11 @@
+DayofWeek date = LocalDate.now().getDayofWeek();
+
+switch (date) {
+    case SATURDAY:
+        System.out.println("Good Saturday");
+        break;
+    case MONDAY:
+        System.out.println("Good Monday")
+        break;
+
+}
